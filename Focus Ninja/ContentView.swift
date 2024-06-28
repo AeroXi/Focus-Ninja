a53726939e95faa0c03f31ab0235887af55aa4e8
AeroXi <magictalentx@gmail.com>
2024-06-28 22:24:03 +0800

    Initial Commit

---
+++ Focus Ninja/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Focus Ninja
+//
+//  Created by Shark on 2024/6/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Focus Ninja/Focus_NinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Focus_NinjaApp.swift
+//  Focus Ninja
+//
+//  Created by Shark on 2024/6/28.
+//
+
+import SwiftUI
+
+@main
+struct Focus_NinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
